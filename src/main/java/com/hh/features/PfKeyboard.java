03a0e3a773c956dbcc1ef86f65b813e53c231012
wajdihh <wajdihh@gmail.com>
2016-02-12 16:18:48 +0100

    correct the bug of show keyboard

--- src/main/java/com/hh/features/PfKeyboard.java
+++ src/main/java/com/hh/features/PfKeyboard.java
@@ -6,9 +6,10 @@ import android.view.inputmethod.InputMethodManager;
 
 public class PfKeyboard {
 
-	public static void show(Context pContext,View pView){
-		InputMethodManager lInputManager = (InputMethodManager) pContext.getSystemService(Context.INPUT_METHOD_SERVICE);
-		lInputManager.showSoftInput(pView, 0);
+	public static void show(Context pContext){
+		((InputMethodManager)pContext.getSystemService(Context.INPUT_METHOD_SERVICE))
+				.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+
 	}
 
 	public static void hide(Context pContext,View pView){
