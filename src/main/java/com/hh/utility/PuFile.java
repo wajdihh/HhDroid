7d0a25ff5d8683612c6827f3a9fbae8a85475cef
wajdihh <wajdihh@gmail.com>
2016-10-07 11:00:11 +0200

    add copyFile method

--- src/main/java/com/hh/utility/PuFile.java
+++ src/main/java/com/hh/utility/PuFile.java
@@ -204,4 +204,27 @@ public class PuFile {
             }
         }
     }
+
+    /**
+     * Copy file
+     * @param source
+     * @param dest
+     * @throws IOException
+     */
+    public static void copyFile(File source, File dest) throws IOException {
+        InputStream input = null;
+        OutputStream output = null;
+        try {
+            input = new FileInputStream(source);
+            output = new FileOutputStream(dest);
+            byte[] buf = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = input.read(buf)) > 0) {
+                output.write(buf, 0, bytesRead);
+            }
+        } finally {
+            input.close();
+            output.close();
+        }
+    }
 }
