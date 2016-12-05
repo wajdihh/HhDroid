7389ab32d9ac1d592f863fbdbd014219f8178307
wajdihh <wajdihh@gmail.com>
2016-12-06 00:10:18 +0100

    integration de HTTP URL 60%

--- src/main/java/com/hh/communication/PcHttpClient2.java
+++ src/main/java/com/hh/communication/PcHttpClient2.java
@@ -1,12 +1,22 @@
 package com.hh.communication;
 
 import com.hh.listeners.MyCallback;
+
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -101,78 +111,84 @@ public class PcHttpClient2 {
 	}
 
 
-	private void applyRequest(String myURL,MyCallback callback,RequestMethod requestMethod) throws IOException {
+	private void applyRequest(String myURL,MyCallback callback,RequestMethod requestMethod) throws IOException  {
 
+		HttpURLConnection conn=null;
 		if(requestMethod==RequestMethod.GET)
 			myURL=myURL+getQuery(params);
 
-		URL url = new URL(myURL);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setReadTimeout(_mConnexionMaxTimeOut);
-		conn.setConnectTimeout(_mConnexionTimeOut);
-		conn.setDoInput(true);
-		conn.setDoOutput(true);
-		conn.setUseCaches(false);
+		try {
+			URL url = new URL(myURL);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setReadTimeout(_mConnexionMaxTimeOut);
+			conn.setConnectTimeout(_mConnexionTimeOut);
+			conn.setDoInput(true);
+			switch (requestMethod){
+				case GET:
+					conn.setDoOutput(false);
+					conn.setRequestMethod("GET");
+					break;
+				case PUT:
+					conn.setDoOutput(true);
+					conn.setRequestMethod("PUT");
+					// for the post and the PUT
+					OutputStream os = conn.getOutputStream();
+					BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
 
-		// for the post and the PUT
-		OutputStream os = conn.getOutputStream();
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
-		// For json Object
-		DataOutputStream printout;
+					writer.write(getQuery(params));
+					writer.flush();
+					writer.close();
+					os.close();
 
+					// For json Object
+					DataOutputStream printout;
+					if(_mJsonObject!=null){
+						// Send POST output.
+						printout = new DataOutputStream(conn.getOutputStream ());
+						printout.writeBytes(URLEncoder.encode(_mJsonObject.toString(),"UTF-8"));
+						printout.flush ();
+						printout.close ();
+					}
+					break;
+				case POST:
+					conn.setDoOutput(true);
+					conn.setRequestMethod("POST");
+					// for the post and the PUT
+					OutputStream os2 = conn.getOutputStream();
+					BufferedWriter writer2 = new BufferedWriter(new OutputStreamWriter(os2, "UTF-8"));
+					writer2.write(getQuery(params));
+					writer2.flush();
+					writer2.close();
+					os2.close();
 
-		switch (requestMethod){
-			case GET:
-				conn.setRequestMethod("GET");
-				break;
-			case PUT:
-				conn.setRequestMethod("PUT");
-				writer.write(getQuery(params));
-				writer.flush();
-				writer.close();
-				os.close();
+					// For json Object
+					DataOutputStream printout2;
+					if(_mJsonObject!=null){
+						// Send POST output.
+						printout2 = new DataOutputStream(conn.getOutputStream ());
+						printout2.writeBytes(URLEncoder.encode(_mJsonObject.toString(),"UTF-8"));
+						printout2.flush ();
+						printout2.close ();
+					}
+					break;
+			}
 
-				// For Json Object
-				if(_mJsonObject!=null){
-					// Send POST output.
-					printout = new DataOutputStream(conn.getOutputStream ());
-					printout.writeBytes(URLEncoder.encode(_mJsonObject.toString(),"UTF-8"));
-					printout.flush ();
-					printout.close ();
-				}
-				break;
-			case POST:
-				// for Params
-				conn.setRequestMethod("POST");
-				writer.write(getQuery(params));
-				writer.flush();
-				writer.close();
-				os.close();
+			for(NameValuePair h : headers)
+				conn.setRequestProperty(h.getName(), h.getValue());
 
-				// For Json Object
-				if(_mJsonObject!=null){
-					// Send POST output.
-					printout = new DataOutputStream(conn.getOutputStream ());
-					printout.writeBytes(URLEncoder.encode(_mJsonObject.toString(),"UTF-8"));
-					printout.flush ();
-					printout.close ();
-				}
-				break;
+			//conn.connect();
+			_mResponseCode =conn.getResponseCode();
+			_mMessageStatus =conn.getResponseMessage();
+			_mResponse = convertStreamToString(conn.getInputStream());
+
+			if(callback!=null) callback.onSuccess(_mResponse);
+		}catch (IOException e) {
+			throw new IOException(e);
+		}  finally {
+			if ( conn!= null) {
+				conn.disconnect();
+			}
 		}
-
-		for(NameValuePair h : headers)
-		{
-			conn.setRequestProperty(h.getName(), h.getValue());
-		}
-
-		conn.connect();
-		_mResponseCode =conn.getResponseCode();
-		_mMessageStatus =conn.getResponseMessage();
-		_mResponse = convertStreamToString(conn.getInputStream());
-
-		conn.disconnect();
-
-		if(callback!=null) callback.onSuccess(_mResponse);
 	}
 
 
@@ -198,45 +214,10 @@ public class PcHttpClient2 {
 		return sb.toString();
 	}
 
-	public void sendMultipartImage(String url,File imageFile,String multipartKeyName){
+	public void sendMultipartImage(String myURL,File imageFile,String multipartKeyName){
 
-//		HttpPost httppost = new HttpPost(url);
-//		//add headers
-//		for(NameValuePair h : headers)
-//		{
-//			httppost.addHeader(h.getName(), h.getValue());
-//		}
-//
-//		httppost.removeHeaders("Content-Type");
-//
-//
-//		MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
-//
-//		try {
-//			BitmapFactory.Options options = new BitmapFactory.Options();
-//			options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-//			Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath(), options);
-//
-//			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-//			bitmap.compress(Bitmap.CompressFormat.JPEG, 75, bos);
-//			byte[] data = bos.toByteArray();
-//			ByteArrayBody bab = new ByteArrayBody(data, imageFile.getName());
-//			reqEntity.addPart(multipartKeyName, bab);
-//
-//			httppost.setEntity(reqEntity);
-//			HttpResponse response = httpClient.execute(httppost);
-//			BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-//			String sResponse;
-//			StringBuilder s = new StringBuilder();
-//			while ((sResponse = reader.readLine()) != null) {
-//				s = s.append(sResponse);
-//			}
-//			_mResponseCode = response.getStatusLine().getStatusCode();
-//			_mMessageStatus = response.getStatusLine().getReasonPhrase();
-//			_mResponse=s.toString();
-//		}catch (Exception e) {
-//			Log.e("EX", e.getLocalizedMessage(), e);
-//		}
+
+
 
 	}
 
